674b33471711cd1ac06e26607ca8da4557b88f98
Kamil Wojdyło <kamil.wojdylo@vazco.eu>
2016-04-21 16:04:27 +0200

    Updates package to be compatibile with Meteor 1.3.2.3

--- package.js
+++ package.js
@@ -1,24 +1,18 @@
 Package.describe({
     name: 'universe:admin',
     summary: 'Admin panel for UniCMS.',
-    version: '0.1.0',
+    version: '0.2.0',
     readme: 'README.md'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('METEOR@1.2.1');
+    api.versionsFrom('1.3.2.3');
 
     api.use([
         'modules@0.5.0-modules.8',
         'ecmascript@0.4.0-modules.8',
         'universe:ui-react@0.1.0',
         'universe:ui-react-menu@0.1.0',
-    ]);
-
-    api.addFiles([
-        'index.js',
-        'lib/AdminMenu.jsx',
-        'components/AdminPanel.jsx'
     ]);
 
     api.mainModule('./index.js');
